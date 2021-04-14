3550e42cf257a57934687219ffa4228a6834de64
Ahmad Hasnain <ahmad.husnain@pf.com.pk>
2021-04-14 14:48:14 +0500

    message change stream modified

--- changeStream/message.js
+++ changeStream/message.js
@@ -64,7 +64,7 @@ const message = (conn, io) => {
                         saveMessageEmits({message_token:change._id,type:ids.type,company_id:ids.company_id,team_id:ids.team_id,channel_id:id,data:messageTemp,emit_to:id,emit_name:"forwardMessage"});
                     }
                     else if(messageTemp.send_after){
-                        let send_after_emit_name = messageTemp.replying_id ? "replySendAfterMessage" : "newMessage";
+                        let send_after_emit_name = messageTemp.replying_id ? "newReplyMessage" : "newMessage";
                         let id=messageTemp.channel_id;
                         delete messageTemp.channel_id;
                         io.to(messageTemp.sender_id).emit(send_after_emit_name, {message_token:change._id,type:ids.type,company_id:ids.company_id,team_id:ids.team_id,channel_id:id,data:messageTemp});
@@ -91,8 +91,8 @@ const message = (conn, io) => {
                     break;
                 case "update":
                     let messageUpdateCheck = change.updateDescription.updatedFields;
-                    let send_after_emit_name = messageUpdateCheck.replying_id ? "replySendAfterMessage" : "sendAfterMessage";
-                    let update_message_emit_name = messageUpdateCheck.replying_id ? "replyUpdateMessage" : "updateMessage";
+                    let send_after_emit_name = messageTemp.replying_id ? "replySendAfterMessage" : "sendAfterMessage";
+                    let update_message_emit_name = messageTemp.replying_id ? "replyUpdateMessage" : "updateMessage";
                     if(messageUpdateCheck.send_after===null){
                         io.to(messageTemp.channel_id).emit(send_after_emit_name, {message_token:change._id,type:ids.type,company_id:ids.company_id,team_id:ids.team_id,channel_id:messageTemp.channel_id,data:messageTemp});
                         saveMessageEmits({message_token:change._id,type:ids.type,company_id:ids.company_id,team_id:ids.team_id,channel_id:messageTemp.channel_id,data:messageTemp,emit_to:messageTemp.channel_id,emit_name:send_after_emit_name});
