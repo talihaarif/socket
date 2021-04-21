a4e0080839b1176ece3724f7219184cfc415093f
hisham <khawaja.hisham@pf.com.pk>
2021-04-21 16:49:44 +0500

    Comment remove

--- routes/api/sanitize.js
+++ routes/api/sanitize.js
@@ -6,18 +6,7 @@ const router = expess.Router();
 router.post("/sanitizeMessage", async (req, res) => {
     try{
         let html = req.body;
-        // console.log(html)
         printList(html);
-        // req.body.message = sanitizeHtml(req.body.message,{allowedAttributes: {
-        //     'span': [ 'class','data-mention-id' ]
-        // },});
-        // req.body.abc = sanitizeHtml(req.body.abc,{allowedAttributes: {
-        //     'span': [ 'class','data-mention-id' ]
-        // },});
-        // console.log(req.body.message);
-        // console.log(req.body.abc);
-
-        // return res.json(req.body);
         return res.json(html);
     } catch(err) {
         console.log(err);
