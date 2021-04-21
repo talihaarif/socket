d1b23cbfea6ab0983098bfaad494b1a62bfe2323
hisham <khawaja.hisham@pf.com.pk>
2021-04-21 09:07:00 +0500

    message update

--- changeStream/message.js
+++ changeStream/message.js
@@ -121,7 +121,6 @@ const message = (conn, io) => {
                             io.to(messageTemp.channel_id).emit(update_message_emit_name, { message_token: change._id, type: ids.type, company_id: ids.company_id, team_id: ids.team_id, channel_id: messageTemp.channel_id, data: messageTemp });
                             messageTemp.replying_id ? "" : saveMessageEmits({ message_token: change._id, type: ids.type, company_id: ids.company_id, team_id: ids.team_id, channel_id: messageTemp.channel_id, data: messageTemp, emit_to: messageTemp.channel_id, emit_name: update_message_emit_name });
                         }
-                        
                     }
                     break;
             }
