8cb9274cdaa41ba9ae049d28134c437ad7c16200
hisham <khawaja.hisham@pf.com.pk>
2021-04-30 15:45:50 +0500

    team create fix

--- utils/team.js
+++ utils/team.js
@@ -81,6 +81,7 @@ const teamInsert=async(teamTemp,io,resumeToken)=>{
             const result =await axios.post(url+"api/teamData", body, configuration);
             console.log("result of team",result.data);
             createTeamRoom(io,result.data);
+            createPublicPrivateChannelRoom(io, result.data);
             io.to(result.data.user_id).emit("newTeamCreated",{company_id:result.data.company_id,team:result.data.team, public:result.data.public , private:result.data.private , team_token:resumeToken});
             saveTeamEmits({company_id:result.data.company_id,team:result.data.team, public:result.data.public , private:result.data.private ,team_token:resumeToken,emit_to:result.data.user_id,emit_name:"newTeamCreated"});
             result.data.sub_admins.map(async(user_id)=>{
