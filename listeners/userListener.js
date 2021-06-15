f9b528c1819c1a2fc9ccfb3afeab1b3ce42c1cdc
Ahmad Hasnain <ahmad.husnain@pf.com.pk>
2021-06-15 16:29:00 +0500

    consoles added

--- listeners/userListener.js
+++ listeners/userListener.js
@@ -61,6 +61,7 @@ const userAllowAccessModified = async(data,io) =>{
     let body1 = JSON.stringify({ company_id, attribute:"company_member", operation:"update" });
     let result1 = await axios.post(url+"api/getSubAdmins", body1, configuration);
     result1.data.sub_admins.push(result1.data.admin);
+    console.log("sub_admins are: ",result1.data.sub_admins );
     for (let user_id of result1.data.sub_admins){
         io.to(user_id).emit("userAllowAccessModified", {company_id:data.company_id,user_id:data.user_id, allow_access:data.allow_access,hash:hash});
     }
